32d79cc38a685b9e64064dc469f90d47dfb933ed
Olmioris <olmioris@gmail.com>
2026-07-08 12:50:54 +0300

    Update align_labels.py

--- src/modernbert/align_labels.py
+++ src/modernbert/align_labels.py
@@ -1,1 +1,22 @@
+def align_labels(example, tokenizer, max_length=192):
+    tokenized = tokenizer(
+        example["tokens"],
+        is_split_into_words=True,
+        truncation=True,
+        max_length=max_length
+    )
 
+    labels = []
+    word_ids = tokenized.word_ids()
+
+    for i, w in enumerate(word_ids):
+        if w is None:
+            labels.append(-100)
+        else:
+            if i == 0 or w != word_ids[i - 1]:
+                labels.append(example["ner_tags"][w])
+            else:
+                labels.append(-100)
+
+    tokenized["labels"] = labels
+    return tokenized
