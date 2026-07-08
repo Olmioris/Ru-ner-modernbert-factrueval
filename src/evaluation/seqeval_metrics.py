6ab844fe0d24ed1a862b86c52d168a59ac4bb8a9
Olmioris <olmioris@gmail.com>
2026-07-08 13:44:55 +0300

    Update seqeval_metrics.py

--- src/evaluation/seqeval_metrics.py
+++ src/evaluation/seqeval_metrics.py
@@ -1,10 +1,9 @@
-from seqeval.metrics import precision_score, recall_score, f1_score
+import evaluate
 
-def compute_seqeval_metrics(y_true, y_pred):
+seqeval_metric = evaluate.load("seqeval")
+
+def seqeval_strict_micro_f1(y_true, y_pred):
     """
-    Compute precision, recall, and F1 using seqeval.
+    Compute strict micro F1 using seqeval.
     """
-    precision = precision_score(y_true, y_pred)
-    recall = recall_score(y_true, y_pred)
-    f1 = f1_score(y_true, y_pred)
-    return precision, recall, f1
+    return seqeval_metric.compute(predictions=y_pred, references=y_true, zero_division=0)
