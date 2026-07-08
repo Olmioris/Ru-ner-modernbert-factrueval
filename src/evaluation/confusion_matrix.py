527c8116d3a35e4411f08be1cd3a2366adbb4c98
Olmioris <olmioris@gmail.com>
2026-07-08 13:45:35 +0300

    Update confusion_matrix.py

--- src/evaluation/confusion_matrix.py
+++ src/evaluation/confusion_matrix.py
@@ -1,17 +1,31 @@
-import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-def entity_confusion_matrix(y_true, y_pred, entity_types):
-    """
-    Compute confusion matrix for entity types (PER, ORG, LOC).
-    """
-    matrix = {t: {t2: 0 for t2 in entity_types} for t in entity_types}
+from src.evaluation.spans import bio_to_spans
 
-    for true_seq, pred_seq in zip(y_true, y_pred):
-        for t, p in zip(true_seq, pred_seq):
-            if t != "O" and p != "O":
-                t_type = t.split("-")[-1]
-                p_type = p.split("-")[-1]
-                if t_type in entity_types and p_type in entity_types:
-                    matrix[t_type][p_type] += 1
+def confusion_matrix_by_type(y_true, y_pred, entity_types):
+    labels = list(entity_types)
+    idx = {t: i for i, t in enumerate(labels)}
+    cm = np.zeros((len(labels), len(labels)), dtype=int)
 
-    return pd.DataFrame(matrix).T
+    for gt, pr in zip(y_true, y_pred):
+        gsp = bio_to_spans(gt)
+        psp = bio_to_spans(pr)
+        gb = {(s, e): t for s, e, t in gsp}
+        pb = {(s, e): t for s, e, t in psp}
+        for b in set(gb.keys()) & set(pb.keys()):
+            g = gb[b]
+            p = pb[b]
+            if g in idx and p in idx:
+                cm[idx[g], idx[p]] += 1
+    return cm, labels
+
+def plot_confusion(cm, labels, title):
+    plt.figure(figsize=(5, 4))
+    sns.heatmap(cm, annot=True, fmt="d", cmap="Blues",
+                xticklabels=labels, yticklabels=labels)
+    plt.title(title)
+    plt.xlabel("pred")
+    plt.ylabel("gold")
+    plt.show()
