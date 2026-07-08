26bcc1730198fb9befc9d68aeecce4a87e8c1463
Olmioris <olmioris@gmail.com>
2026-07-08 13:25:26 +0300

    Update merge_datasets.py

--- src/synthetic/merge_datasets.py
+++ src/synthetic/merge_datasets.py
@@ -1,1 +1,13 @@
+from datasets import DatasetDict
 
+def merge_original_and_synthetic(original_ds, synthetic_ds):
+    """
+    Merge original and synthetic datasets for augmented training.
+    """
+    return DatasetDict(
+        {
+            "train": original_ds["train"] + synthetic_ds["train"],
+            "validation": original_ds["validation"],
+            "test": original_ds["test"]
+        }
+    )
